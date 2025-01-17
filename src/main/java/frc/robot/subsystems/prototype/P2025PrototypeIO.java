f29a8b7aee66f53d454f6e4fdda73e58db47d90b
Adriel <stammler.adriel@gmail.com>
2025-01-17 11:58:49 -0800

    New Phoenix.json version
    Updated Pheonix6 up to 25.2.0

--- src/main/java/frc/robot/subsystems/prototype/P2025PrototypeIO.java
+++ src/main/java/frc/robot/subsystems/prototype/P2025PrototypeIO.java
@@ -7,7 +7,6 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.util.Units;
