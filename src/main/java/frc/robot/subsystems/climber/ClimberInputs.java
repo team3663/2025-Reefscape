d72b15266f131dc20f7645607fd0d0b3ab3c226c
Grayson Gile <156486132+Gmag1977@users.noreply.github.com>
2025-01-25 10:17:22 -0800

    Update ClimberInputs.java

--- src/main/java/frc/robot/subsystems/climber/ClimberInputs.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberInputs.java
@@ -5,6 +5,7 @@ import edu.wpi.first.epilogue.Logged;
 @Logged
 public class ClimberInputs {
     public double currentPosition;
+    public double currentVelocity;
     public double currentAppliedVoltage;
     public double motorTemperature;
     public double currentDraw;
