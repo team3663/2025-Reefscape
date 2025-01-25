d2915b80251d19a8ff555dd7927bb4284fc1d13a
Adriel <stammler.adriel@gmail.com>
2025-01-25 14:14:20 -0800

    RobotMode Init Added Changed everything to the Color class by wpilib Added a color logger Added RobotMode Made the LED color be based on RobotMode

---
+++ src/main/java/frc/robot/RobotMode.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public enum RobotMode {
+    CORAL_LEVEL_1,
+    CORAL_LEVEL_2,
+    CORAL_LEVEL_3,
+    CORAL_LEVEL_4,
+    ALGAE_PROCESSOR,
+    ALGAE_NET,
+    ALGAE_REMOVE_LOWER,
+    ALGAE_REMOVE_UPPER
+}
