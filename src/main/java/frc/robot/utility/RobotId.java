e0740f540c0970822dc0ce975d24836232becabb
Jacob Bublitz <Jacob@Bublitz.com>
2025-01-25 11:12:06 -0800

    Fix logging error when detected robot ID is null (#20)

--- src/main/java/frc/robot/utility/RobotId.java
+++ src/main/java/frc/robot/utility/RobotId.java
@@ -1,7 +1,7 @@
 package frc.robot.utility;
 
 public enum RobotId {
-    SIM("00-00-00-00-00-00"),
+    UNKNOWN("00-00-00-00-00-00"),
     C2024("00-80-2f-33-d0-1c"),
     C2025("00-80-2f-33-d0-3f");
 
