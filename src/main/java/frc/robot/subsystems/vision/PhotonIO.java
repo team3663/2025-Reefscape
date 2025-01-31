0b6163984f899afa797567a410a709930757c2cc
Richard Elmore <richardelmore@live.com>
2025-01-30 20:20:39 -0800

    Copy PhotonIO code from last season.

--- src/main/java/frc/robot/subsystems/vision/PhotonIO.java
+++ src/main/java/frc/robot/subsystems/vision/PhotonIO.java
@@ -1,4 +1,55 @@
 package frc.robot.subsystems.vision;
 
-public class PhotonIO {
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PhotonIO implements VisionIO {
+    private final PhotonPoseEstimator estimator;
+
+    public PhotonIO(String cameraName, Transform3d cameraTransform) {
+        PhotonCamera camera = new PhotonCamera(cameraName);
+
+        AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+
+        estimator = new PhotonPoseEstimator(fieldLayout,
+                PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraTransform);
+    }
+
+    public void updateInputs(VisionInputs visionInputs) {
+        // Assume pose will not be updated.
+        visionInputs.poseUpdated = false;
+
+        // Values used by the CLOSEST_TO_LAST_POSE and CLOSEST_TO_REFERENCE_POST strategies.
+        estimator.setLastPose(visionInputs.estimatedPose);
+        estimator.setReferencePose(visionInputs.estimatedPose);
+
+        Optional<EstimatedRobotPose> newPose = estimator.update();
+
+        // If there is no new pose then we have nothing to do, just bail out.
+        if (newPose.isEmpty()) {
+            return;
+        }
+
+        EstimatedRobotPose newEstimate = newPose.get();
+        visionInputs.estimatedPose = newEstimate.estimatedPose.toPose2d();
+        visionInputs.timestampSeconds = newEstimate.timestampSeconds;
+
+        // Get the list of targets used in the current estimate and store their Ids in our Inputs.
+        List<PhotonTrackedTarget> targets = newEstimate.targetsUsed;
+        int[] ids = new int[targets.size()];
+        int index = 0;
+        for (PhotonTrackedTarget target : targets) {
+            ids[index++] = target.getFiducialId();
+        }
+        visionInputs.targetIds = ids;
+        visionInputs.poseUpdated = true;
+    }
 }
