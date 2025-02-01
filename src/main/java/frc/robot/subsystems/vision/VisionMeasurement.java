941ac0b325c9c96fd260f5d609997d95e1fcaf5f
Richard Elmore <richardelmore@live.com>
2025-02-01 12:06:14 -0800

    Fix Epilog issue

--- src/main/java/frc/robot/subsystems/vision/VisionMeasurement.java
+++ src/main/java/frc/robot/subsystems/vision/VisionMeasurement.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.vision;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.numbers.N1;
@@ -10,6 +11,7 @@ import edu.wpi.first.math.numbers.N3;
 public class VisionMeasurement {
     public Pose2d estimatedPose;
     public double timestamp;
+    @NotLogged
     public Matrix<N3, N1> stdDevs;
 
     public VisionMeasurement(Pose2d pose, double timestamp, Matrix<N3, N1> stdDevs) {
