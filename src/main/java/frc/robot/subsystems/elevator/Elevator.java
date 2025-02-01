8c99916992769091a4d1ab33b725c07d8aa9f783
Adriel <stammler.adriel@gmail.com>
2025-01-31 20:22:48 -0800

    Fixed it Also commented

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -79,24 +79,23 @@ public class Elevator extends SubsystemBase {
     }
 
 
-
-
     public Command zero() {
-        return waitUntil(() -> Math.abs(inputs.currentVelocityMotor1) < VELOCITY_THRESHOLD)
-                // Then reset the elevator position
-                .andThen(() -> {
-                    io.resetPosition();
-                    zeroed = true;
-                })
-                // Before we check if we're at the bottom hard stop, wait a little
-                .beforeStarting(waitSeconds(WAIT_TIME))
-                // Retract while we haven't found the bottom hard stop
-                .withDeadline(runEnd(
-                        () -> io.setTargetVoltage(-1.0),
-                        io::stop));
+        // Run the Elevator backwards until stopped and then stop
+        return runEnd(() -> io.setTargetVoltage(-1.0), io::stop)
+                // While doing that wait until the elevator stops (Hit the hard stop)
+                // Also stop the previous command when this one stops (It hit the hard stop and reset position)
+                .withDeadline(waitUntil(() -> Math.abs(inputs.currentVelocityMotor1) < VELOCITY_THRESHOLD)
+                        // Then reset the elevator position and set zeroed to true
+                        .andThen(() -> {
+                            io.resetPosition();
+                            zeroed = true;
+                        })
+                        // Before we check if we're at the bottom hard stop, wait a little so that it doesn't think we hit it because we haven't started going yet
+                        .beforeStarting(waitSeconds(WAIT_TIME)));
     }
 
     public record Constants(
             double maximumPosition
-    ) {}
+    ) {
+    }
 }
