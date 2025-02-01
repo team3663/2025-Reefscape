feb39a1d5c63b5aadf322b7dcf78bf50a0b33dc6
Grayson Gile <156486132+Gmag1977@users.noreply.github.com>
2025-01-31 20:41:16 -0800

    Add command for zeroing the elevator (#24)
    * Zeroing
    
    zeroed the elevator
    
    * removing unnecessary code
    
    removing unnecessary code
    
    * Fixed it
    Also commented
    
    ---------
    
    Co-authored-by: Adriel <stammler.adriel@gmail.com>

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -7,15 +7,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import java.util.function.DoubleSupplier;
 
+import static edu.wpi.first.wpilibj2.command.Commands.waitSeconds;
+import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+
 @Logged
 public class Elevator extends SubsystemBase {
-
-    private static final double POSITION_THRESHOLD = Units.inchesToMeters(1);
+    private static final double WAIT_TIME = 0.25; // In seconds
+    private static final double POSITION_THRESHOLD = Units.inchesToMeters(1.0);
+    private static final double VELOCITY_THRESHOLD = Units.rotationsPerMinuteToRadiansPerSecond(1.0);
 
     private final ElevatorIO io;
     private final ElevatorInputs inputs = new ElevatorInputs();
     private final Constants constants;
 
+    private boolean zeroed = false;
     private double targetPosition = 0.0;
 
     public Elevator(ElevatorIO io) {
@@ -73,7 +78,24 @@ public class Elevator extends SubsystemBase {
         }, io::stop);
     }
 
+
+    public Command zero() {
+        // Run the Elevator backwards until stopped and then stop
+        return runEnd(() -> io.setTargetVoltage(-1.0), io::stop)
+                // While doing that wait until the elevator stops (Hit the hard stop)
+                // Also stop the previous command when this one stops (It hit the hard stop and reset position)
+                .withDeadline(waitUntil(() -> Math.abs(inputs.currentVelocityMotor1) < VELOCITY_THRESHOLD)
+                        // Then reset the elevator position and set zeroed to true
+                        .andThen(() -> {
+                            io.resetPosition();
+                            zeroed = true;
+                        })
+                        // Before we check if we're at the bottom hard stop, wait a little so that it doesn't think we hit it because we haven't started going yet
+                        .beforeStarting(waitSeconds(WAIT_TIME)));
+    }
+
     public record Constants(
             double maximumPosition
-    ) {}
+    ) {
+    }
 }
