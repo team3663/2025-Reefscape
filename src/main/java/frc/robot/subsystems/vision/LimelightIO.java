ab53055d3eda9232026594affe0bc36cd9b6459a
Richard Elmore <richardelmore@live.com>
2025-02-14 01:21:19 -0800

    Update LimelightHelpers and set IMU mode.

--- src/main/java/frc/robot/subsystems/vision/LimelightIO.java
+++ src/main/java/frc/robot/subsystems/vision/LimelightIO.java
@@ -6,6 +6,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotState;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
 
@@ -32,20 +33,33 @@ public class LimelightIO implements VisionIO {
         // Assume pose will not be updated.
         visionInputs.poseUpdated = false;
 
+        // When the robot is disabled then seed the limelight's IMU with data from the Pigeon but once
+        // the robot is enabled then switch to the Limelight's internal IMU.
+        if (RobotState.isDisabled()) {
+            // Seed the limelights IMU with yaw values from external IMU (Pigeon)
+            LimelightHelpers.SetIMUMode(cameraName, 1);
+        }
+        else {
+            LimelightHelpers.SetIMUMode(cameraName, 2);
+        }
+
+        // Give the Limelight our current robot yaw as provided by the IMU.
         LimelightHelpers.SetRobotOrientation(cameraName, Units.radiansToDegrees(currentYaw), 0, 0, 0, 0, 0);
-        LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
+
+        // Get a new post estimate
+        LimelightHelpers.PoseEstimate estimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
 
         // If no tags were seen then return without doing anything.
-        if ( mt2.tagCount == 0)
+        if ( estimate.tagCount == 0)
             return;
 
-        visionInputs.estimatedPose = mt2.pose;
-        visionInputs.timestampSeconds = mt2.timestampSeconds;
+        visionInputs.estimatedPose = estimate.pose;
+        visionInputs.timestampSeconds = estimate.timestampSeconds;
 
         // Extract list list of AprilTag Ids see in this pose estimate.
-        int[] targetIds = new int[mt2.rawFiducials.length];
+        int[] targetIds = new int[estimate.rawFiducials.length];
         int index = 0;
-        for (LimelightHelpers.RawFiducial tag: mt2.rawFiducials ) {
+        for (LimelightHelpers.RawFiducial tag: estimate.rawFiducials ) {
             targetIds[index++] = tag.id;
         }
         visionInputs.targetIds = targetIds;
