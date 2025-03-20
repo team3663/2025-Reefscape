aa4687a3318d4ce464842ec7a15a2e818280a894
Adriel <stammler.adriel@gmail.com>
2025-03-19 19:59:27 -0700

    Revert "Removed Grabber supplyCurrentLimit and increased the Algae holding voltage"
    This reverts commit df2625783516cbba41c56972237a9f9a42d0fb06.

--- src/main/java/frc/robot/subsystems/grabber/C2025GrabberIO.java
+++ src/main/java/frc/robot/subsystems/grabber/C2025GrabberIO.java
@@ -33,6 +33,8 @@ public class C2025GrabberIO implements GrabberIO {
         TalonFXConfiguration motorConfig = new TalonFXConfiguration();
         motorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
         motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motorConfig.CurrentLimits.SupplyCurrentLimit = 15.0;
+        motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
 
         motor.getConfigurator().apply(motorConfig);
     }
