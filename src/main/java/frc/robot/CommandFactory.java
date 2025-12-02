a9234503211f9b702a0fb8714108e5d7b3d4bf46
Adriel <stammler.adriel@gmail.com>
2025-12-01 19:04:42 -0800

    Oops, the prev commit needed this import too

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
